40bdedcc5adb6895a383b962bfe25d08c57b0d80
pabluchenko <jp.junco@uniandes.edu.co>
2024-10-27 22:38:21 -0500

    fin

--- config.py
+++ config.py
@@ -5,9 +5,17 @@ import os
 load_dotenv()  
 
 class Settings(BaseSettings):
-    SECRET_KEY: str = os.getenv('SECRET_KEY')
-    DATABASE_URL: str = os.getenv('DATABASE_URL')
-    JWT_SECRET_KEY: str = os.getenv('JWT_SECRET_KEY')
+    SECRET_KEY: str = os.getenv('SECRET_KEY', 'your-secret-key')
+    JWT_SECRET_KEY: str = os.getenv('JWT_SECRET_KEY', 'your-jwt-secret-key')
+    DB_USER: str = os.getenv('DB_USER', 'postgres')
+    DB_PASSWORD: str = os.getenv('DB_PASSWORD', 'password')
+    DB_HOST: str = os.getenv('DB_HOST', 'database-1.chi2qeoo0z9n.us-east-1.rds.amazonaws.com')
+    DB_PORT: str = os.getenv('DB_PORT', '5432')
+    DB_NAME: str = os.getenv('DB_NAME', 'blacklist_db')
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
     class Config:
         env_file = ".env"
