def182b51ebb21166ab265b091894f6e0007e9ae
waponte <w.aponte@uniandes.edu.co>
2024-11-17 16:26:46 -0500

    add health check

--- app/routes/blacklist_route.py
+++ app/routes/blacklist_route.py
@@ -8,6 +8,11 @@ from fastapi_jwt_auth import AuthJWT
 
 router = APIRouter()
 
+# Health Check Route
+@router.get("/health", status_code=status.HTTP_200_OK)
+async def health_check():
+    return {"status": "healthy"}
+
 @router.post("/", response_model=dict, status_code=status.HTTP_201_CREATED)
 async def add_to_blacklist(
     blacklist_data: BlacklistCreate,
